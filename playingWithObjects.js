b245d5fc8a5a1429ea25494c07699d5e39028151
smilliononline <million_18@outlook.com>
2018-05-24 21:12:39 -0500

    update

--- playingWithObjects.js
+++ playingWithObjects.js
@@ -10,9 +10,8 @@ age: 27}
 console.log(users[1].age);
 console.log(users[0].name);
 
-fu
-for(name, age in users){
-    console.log(users[name], " - " + users[age]);
+var users = [{name: "Michael", age:37}, {name: "John", age:30}, {name: "David", age:27}];
+
+for(let person of users){
+    console.log(person.name + " - " + person.age);
 }
-    
-/* I was unable to get the for loop to work pulling variables in the object, only one, so I would have to ask for some help understanding that.*/
